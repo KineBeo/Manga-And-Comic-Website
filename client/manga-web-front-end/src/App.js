2a91a6e04deb5c24804a60554b84a40fdf1689d2
TruongHoang2004 <22028035@vnu.edu.vn>
2024-03-20 21:36:42 +0700

    First commit

--- client/manga-web-front-end/src/App.js
+++ client/manga-web-front-end/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="bg-green-400 p-5 m-4 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold underline">Manga Web Front End</h1>
       <p className="text-lg">This is the front end for the manga web app</p>
-      <p>Test gitignore </p>
+      <p>Không biết viết gì =)) </p>
     </div>
   );
 }
